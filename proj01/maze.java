6c03a497cad7e1438e4f0e7342e4ce126d7735a9
Matt Moretti <werebus@gmail.com>
2011-09-15 21:54:21 -0400

    toString for Maze

--- proj01/maze.java
+++ proj01/maze.java
@@ -1,6 +1,21 @@
 public class Maze {
-  int width;
-  int height;
+  public int width;
+  public int height;
 
   Cell[][] cells;
+
+  public String toString() {
+    String result = new String("");
+
+    for (int x = 0; x < width; x++) {
+      for (int y = 0; y < height; y++) {
+         result += cells[x][y].toShortString();
+      }
+      if (x < width - 1) {
+        result += "\r";
+      }
+    }
+
+    return result;
+  }
 }
