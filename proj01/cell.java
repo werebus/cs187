f9c55324581697b33371f203d656c2821a009ead
Matt Moretti <werebus@gmail.com>
2011-09-15 21:52:57 -0400

    Constructors and toString-type methods

--- proj01/cell.java
+++ proj01/cell.java
@@ -1,4 +1,36 @@
 public class Cell {
-  int x;
-  int y;
-  boolean isOpen;
+  public int x;
+  public int y;
+  public boolean open;
+
+  Cell (int xVal, int yVal, boolean isOpen) {
+    x = xVal;
+    y = yVal;
+    open = isOpen;
+  }
+
+  Cell (int xVal, int yVal) {
+    this(xVal, yVal, true);
+  }
+  
+  public String coords() {
+    return "(" + Integer.toString(x) + ", " + Integer.toString(y) + ")"
+  }
+
+  public String toString() {
+    if (open) {
+      return coords() + " open";
+    } else {
+      return coords() + " closed";
+    }
+  }
+
+  public String toShortString() {
+    if (open) {
+      return "1";
+    } else {
+      return "0";
+    }
+  }
+  
+}
