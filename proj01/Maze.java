e3b416da5b79d902b971b8347167f4e2099e2744
Matt Moretti <werebus@gmail.com>
2011-09-18 15:14:59 -0400

    Maze constructor

--- proj01/Maze.java
+++ proj01/Maze.java
@@ -4,6 +4,27 @@ public class Maze {
 
   Cell[][] cells;
 
+  Maze (int w, int h, String[] init) {
+    //TODO: Some error checking here. init should hava a size of h, and all
+    //elements of init should have w characters. init should have only 1's
+    //and 0's in it.
+    boolean open;
+    for (int y = 0; y < h; y++) {
+      for (int x = 0; x < w; x++) {
+        open = (init[y].charAt(x) != '0');
+        cells[x][y] = new Cell(x, y, open);
+      }
+    }
+  }
+
+  Maze (int w, int h) {
+    for (int y = 0; y < h; y++) {
+      for (int x = 0; x < w; x++) {
+        cells[x][y] = new Cell(x, y, true);
+      }
+    }
+  }
+
   public int getWidth() {
     return width;
   }
