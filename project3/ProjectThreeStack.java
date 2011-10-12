2709c45aeca5503b6fd38f5509068a06474816c1
Matt Moretti <werebus@gmail.com>
2011-10-11 22:37:09 -0400

    Why use top when we have size()?  That should always be fine.

--- project3/ProjectThreeStack.java
+++ project3/ProjectThreeStack.java
@@ -1,5 +1,6 @@
 public class ProjectThreeStack<T> extends DropoutStack<T> {
 
+  //exactly the same constructors as DropoutStack
   public ProjectThreeStack() {
     super();
   }
@@ -8,8 +9,10 @@ public class ProjectThreeStack<T> extends DropoutStack<T> {
     super(c);
   }
 
+  //before we push, check to see if our size is equal to the capacity
+  //of the stack.  If so, resize the stack to twice it's size.
   public void push (T element) {
-    if ((getTop() + 1) == getCapacity())
+    if (size() == getCapacity())
       resize(getCapacity() * 2);
 
     super.push(element);
